e83af7c59b528000524cffe225204caaf851335a
Vidi <david.heams@googlemail.com>
2016-11-15 11:11:06 +0000

    Cleanup and additions
    Cleaned up spacing.  Added mixins for perspective and origin.

--- gruntfile.js
+++ gruntfile.js
@@ -1,102 +1,102 @@
 module.exports = function (grunt) {
-  'use strict';
+	'use strict';
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-    watch: {
-      sass: {
-        files: ['./css/style.scss'],
-        tasks: ['sass:dev', 'autoprefixer:main'],
-        options: {
-          livereload: true
-        }
-      },
-      js: {
-        files: ['./js/app.js'],
-        tasks: ['uglify:dev'],
-        options: {
-          livereload: true
-        }
-      }
-    },
+		watch: {
+			sass: {
+				files: ['./css/style.scss'],
+				tasks: ['sass:dev', 'autoprefixer:main'],
+				options: {
+					livereload: true
+				}
+			},
+			js: {
+				files: ['./js/app.js'],
+				tasks: ['uglify:dev'],
+				options: {
+					livereload: true
+				}
+			}
+		},
 
-    sass: {
-      dev: {
-        options: {
-          outputStyle: 'compressed',
-          sourceMap: true
-        },
-        files: {
-          './css/style.min.css': './css/style.scss'
-        }
-      },
-      dist: {
-        options: {
-          outputStyle: 'compressed',
-          sourceMap: false
-        },
-        files: {
-          './css/style.min.css': './css/style.scss'
-        }
-      }
-    },
+		sass: {
+			dev: {
+				options: {
+					outputStyle: 'compressed',
+					sourceMap: true
+				},
+				files: {
+					'./css/style.min.css': './css/style.scss'
+				}
+			},
+			dist: {
+				options: {
+					outputStyle: 'compressed',
+					sourceMap: false
+				},
+				files: {
+					'./css/style.min.css': './css/style.scss'
+				}
+			}
+		},
 
-    autoprefixer: {
-      main: {
-        options: {
-          map: true, 
-          browsers: ['ie >= 8', '> 2%']
-        }, 
-        src: './css/style.min.css',
-        dest: './css/style.min.css'
-      }
-    },
+		autoprefixer: {
+			main: {
+				options: {
+					map: true,
+					browsers: ['ie >= 8', '> 2%']
+				},
+				src: './css/style.min.css',
+				dest: './css/style.min.css'
+			}
+		},
 
-    uglify: {
-      dev: {
-        files: {
-          'js/min/app.min.js': ['js/app.js']
-        },
-        options: {
-          sourceMap: true
-        }
-      },
-      dist: {
-        files: {
-          'js/min/app.min.js': ['js/app.js']
-        },
-        options: {
-          sourceMap: false
-        }
-      }
-    },
+		uglify: {
+			dev: {
+				files: {
+					'js/min/app.min.js': ['js/app.js']
+				},
+				options: {
+					sourceMap: true
+				}
+			},
+			dist: {
+				files: {
+					'js/min/app.min.js': ['js/app.js']
+				},
+				options: {
+					sourceMap: false
+				}
+			}
+		},
 
-    copy: {
-      dist: {
-        files: [
-          {
-            expand: true,
-            cwd: './',
-            src: ['js/min/app.min.js', 'index.html', 'css/style.min.css', 'assets/*'],
-            dest: 'dist/'
+		copy: {
+			dist: {
+				files: [
+					{
+						expand: true,
+						cwd: './',
+						src: ['js/min/app.min.js', 'index.html', 'css/style.min.css', 'assets/*'],
+						dest: 'dist/'
           }
         ]
-      }
-    }
+			}
+		}
 
-  });
+	});
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', [
+	grunt.registerTask('default', [
     'watch'
-  ]);
-  grunt.registerTask('dist', [
+	]);
+	grunt.registerTask('dist', [
     'sass:dist', 'uglify:dist', 'autoprefixer:main', 'copy:dist'
-  ]);
+	]);
 };
